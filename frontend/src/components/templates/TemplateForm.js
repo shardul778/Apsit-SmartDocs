24e15b0338f6cf4b675a0582eb7978a579da7b52
Dhananjay Agarwal <dhananjay.ag007@gmail.com>
2025-08-25 01:47:22 +0530

    Update TemplateForm.js

--- frontend/src/components/templates/TemplateForm.js
+++ frontend/src/components/templates/TemplateForm.js
@@ -895,8 +895,10 @@ const TemplateForm = () => {
                              component="img" 
                              src="/DS_header.png" 
                              alt="APSIT Header" 
-                             sx={{
-                              width: 1000,
+                             sx={{ 
+                               width: 200, 
+                               height: 60, 
+                               objectFit: 'contain',
                                filter: 'drop-shadow(0 1px 2px rgba(0,0,0,0.1))'
                              }} 
                            />
